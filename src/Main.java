356e9dd66ed2a1af4f7bfaa4f3b09cc075f539c4
IvanDzundza <doker2008@gmail.com>
2022-10-27 16:29:46 +0300

    Change Menu

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,63 @@
-
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int scan=1;
-        System.out.println("Введіть число для вибору дії:");
-        while (scan!=7){
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> HomeAnimalsNames = new ArrayList<String>();
 
-            scan=1;
-            scan = scanner.nextInt();
-            switch (scan) {
-                case 1:
-                    // Кейс для виводу списку
-                    break;
-                case 2:
-                    // Кейс для додавання інформації в список
-                    break;
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-                case 3:
-                    // Кейс для видалення інформації зі списку
-                    break;
+        System.out.println("Enter the name of animal №1");
+        HomeAnimalsNames.add(bufferedReader.readLine());
+        System.out.println("Enter the name of animal №2");
+        HomeAnimalsNames.add(bufferedReader.readLine());
+        System.out.println("Enter the name of animal №3");
+        HomeAnimalsNames.add(bufferedReader.readLine());
 
-                case 4:
-                    // Кейс для пошуку інформації зі списку
-                    break;
+        String animalName1 = HomeAnimalsNames.get(0);
+        System.out.println("animalName1 = " + animalName1);
 
-                case 5:
-                    // Кейс для виведення пункту "help"
-                    break;
+        String animalName2 = HomeAnimalsNames.get(1);
+        System.out.println("animalName2 = " + animalName2);
 
-                default:
-                    System.out.println("Ви ввели невіне значення");
-                    //зробити перехід на кейс 5
+        String animalName3 = HomeAnimalsNames.get(2);
+        System.out.println("animalName3 = " + animalName3);
+
+
+        boolean quit = false;
+        int menuItem;
+        do {
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.println("MENU Home Animal");
+                for (int i = 1; i <= 3; i++)
+
+                System.out.println(i + ". Amimal name #" + i);
+                System.out.println("0. Quit");
+
+                System.out.println("Chose animal name: ");
+                menuItem = in.nextInt();
+
+                switch (menuItem) {
+                    case 1:
+                        System.out.println("animalName1 = " + animalName1);
+                        break;
+                    case 2:
+                        System.out.println("animalName2 = " + animalName2);
+                        break;
+                    case 3:
+                        System.out.println("animalName3 = " + animalName3);
+                        break;
+                    case 0:
+                        quit = true;
+                        break;
+                } } catch (Exception e) {
+                System.err.println("Please enter a number from 1 to 3");
             }
-        }
+        } while (!quit) ;
+        System.out.println("Good bye!");
     }
 }
